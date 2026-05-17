c1300b2fa1d9f81326d084e9f2f9d7da8b151e47
Anton Liauchuk <anton93lev@gmail.com>
2026-05-17 13:37:59 +0300

    ReverseLinkedList

---
+++ leetcode-221-maximal-square/src/main/java/dsa/ReverseLinkedList.java
@@ -0,0 +1,34 @@
+package dsa;
+
+class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode current = head, previous = null, next;
+
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
