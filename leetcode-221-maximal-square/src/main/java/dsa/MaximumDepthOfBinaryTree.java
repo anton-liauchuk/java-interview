7ef958a3ad9bac5e3a6c4f28f085d147460a24bf
Anton Liauchuk <anton93lev@gmail.com>
2026-05-20 16:55:19 +0300

    MaximumDepthOfBinaryTree

---
+++ leetcode-221-maximal-square/src/main/java/dsa/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,32 @@
+package dsa;
+
+class MaximumDepthOfBinaryTree {
+
+    int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
